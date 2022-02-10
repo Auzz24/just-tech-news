ba9056588658dbf32eb109e0fd8c8fa91b30e265
Austin Miller <austin.miller1424@gmail.com>
2022-02-10 11:14:19 -0600

    passsword hash

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./config/connection');
+const sequelize = require('./seqelize');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
