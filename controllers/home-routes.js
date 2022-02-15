d71506c0c2d3b888da329e1cca73fd0d10559531
Austin Miller <austin.miller1424@gmail.com>
2022-02-15 16:22:37 -0600

    fixed bug

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,7 +30,7 @@ router.get('/', (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      console.log("THESE ARE THE POSTS" + posts)
+      console.log("THESE ARE THE POSTS" + dbPostData)
       res.render('homepage', { posts });
     })
     .catch(err => {
